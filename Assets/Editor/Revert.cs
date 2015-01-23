a8b56ebb22d13977fd1f5b0f4adeccd999b35c98
Hugbot <Hugbot@Hugbots-MBP.netgear.com>
2015-01-23 01:59:31 -0800

    Added placeholder files to get folder structure ready.

---
+++ Assets/Editor/Revert.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PrefabEditorMenu {
+    [MenuItem ("Tools/Revert to Prefab %r")]
+    public static void Revert() {
+        GameObject[] selection = Selection.gameObjects;
+        if(selection.Length > 0) {
+            for (int i = 0; i < selection.Length; i++) {
+                // EditorUtility.ResetGameObjectToPrefabState(selection[i]);
+                PrefabUtility.RevertPrefabInstance(selection[i]);
+            }
+        }
+        else {
+            Debug.Log("Cannot revert to prefab - nothing selected");
+        }
+    }
+}
