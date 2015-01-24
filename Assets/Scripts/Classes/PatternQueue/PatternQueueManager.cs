497d0617bca20d968fa9286633efe752633c0d14
Hugbot <Hugbot@Hugbots-MacBook-Pro.local>
2015-01-24 12:06:40 -0800

    Moved sound files to sound folder, added pattern queue logic basis.

---
+++ Assets/Scripts/Classes/PatternQueue/PatternQueueManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PatternQueueManager : MonoBehaviour {
+
+	//Probably doesn't need to be a singleton
+	//BEGINNING OF SINGLETON CODE CONFIGURATION
+	private static volatile PatternQueueManager _instance;
+	private static object _lock = new object();
+	public PatternQueue patternQueue;
+
+	//Stops the lock being created ahead of time if it's not necessary
+	static PatternQueueManager() {
+	}
+
+	public static PatternQueueManager Instance {
+		get {
+			if(_instance == null) {
+				lock(_lock) {
+					if (_instance == null) {
+						GameObject PatternQueueManagerGameObject = new GameObject("PatternQueueManagerGameObject");
+						_instance = (PatternQueueManagerGameObject.AddComponent<PatternQueueManager>()).GetComponent<PatternQueueManager>();
+					}
+				}
+			}
+			return _instance;
+		}
+	}
+
+	private PatternQueueManager() {
+	}
+
+	public void Awake() {
+		_instance = this;
+	}
+	//END OF SINGLETON CODE CONFIGURATION
+
+	public void Start() {
+	}
+
+	public void Update() {
+		PerformDebugKeys();
+	}
+
+	public void PerformDebugKeys() {
+		if(Input.GetKeyDown(KeyCode.G)) {
+			Debug.Log("Generating Pattern In Queue");
+			GetPatternQueue().AddAtFront(Pattern.Block);
+		}
+	}
+
+	public PatternQueue GetPatternQueue() {
+		return patternQueue;
+	}
+}
