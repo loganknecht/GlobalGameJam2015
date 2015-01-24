d23533e6af80fec17632c646c0a56f9c7c4623e8
Cheezmeister <@gmail>
2015-01-24 13:55:20 -0800

    Add lost files

--- Assets/Scripts/Classes/Tiles/SpikeTile.cs
+++ Assets/Scripts/Classes/Tiles/SpikeTile.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class SpikeTile : RunnerTile
 {
+	public GameObject spikesPrefab;
+
 	protected override void OnActivation() {
 		Debug.Log ("Spikolicious");
+
+		Object.Instantiate (spikesPrefab, this.transform.position + new Vector2(0, 1));
 	}
 }
 
---
+++ Assets/Scripts/Classes/Tiles/TouchActivated.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchActivated : MonoBehaviour {
+
+	public bool HasBeenActivated { get; private set; }
+
+	public void Reset() {
+		HasBeenActivated = false;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("Mouse down received");
+		HasBeenActivated = true;
+	}
+}
