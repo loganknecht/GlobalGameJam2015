1c5b2b7a3f144c5fa7f779090a7947365fd754f4
Cheezmeister <@gmail>
2015-01-24 22:30:13 -0800

    Selection based on pattern matching

--- Assets/Scripts/Classes/Tiles/RunnerTile.cs
+++ Assets/Scripts/Classes/Tiles/RunnerTile.cs
@@ -3,24 +3,46 @@ using System.Collections;
 
 public class RunnerTile : Tile {
 
-  public bool HasBeenActivated { get; set; }
+    public bool HasBeenActivated {
+        get;
+        set;
+    }
 
-	public TouchActivated touchActivated;
+    public Pattern Pattern {
+        get;
+        set;
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	public override void Update () {
-		if (this.touchActivated.HasBeenActivated) {
-			OnActivation();
-			this.touchActivated.Reset();
-		}
-	}
+    public TouchActivated touchActivated;
 
-  	protected virtual void OnActivation() {
-		Debug.Log ("Mousetastic");
-  	}
+    private PatternQueue PQ {
+        get {
+            return PatternQueueManager.Instance.patternQueue;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        var values = Pattern.GetValues (Pattern.GetType ());
+        int num = (int)(Random.value * values.Length);
+		Pattern = (Pattern)values.GetValue (num);	
+        Debug.Log ("I chose pattern " + this.Pattern);
+    }
+
+    // Update is called once per frame
+    public override void Update () {
+        if (this.touchActivated.HasBeenActivated) {
+            if (PQ.ContainsPattern(this.Pattern)) {
+				SoundManager.Instance.Play(AudioType.Correct1);
+                OnActivation();
+            } else {
+				SoundManager.Instance.Play(AudioType.Correct2);
+			}
+            this.touchActivated.Reset();
+        }
+    }
+
+    protected virtual void OnActivation() {
+        Debug.Log ("Mousetastic");
+    }
 }
